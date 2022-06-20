684f946f94ca1518508b99f46ba9e37e329e33a5
Anton Kiyan <kiyananton@gmail.com>
2022-06-21 01:08:36 +0300

    new content

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,11 @@
 import 'jquery-ui-bundle'
+import 'inputmask/dist/jquery.inputmask'
 
 import './select'
+import './modal'
+import './phone-input'
 
 import './nav'
 import './quality-tabs'
 import './about-brand-tabs'
+import './modal-partnership'
--- source/js/select.js
+++ source/js/select.js
@@ -2,10 +2,21 @@ import select2 from 'select2'
 import niceScroll from 'jquery.nicescroll'
 
 $(window).on('load', function () {
-  $('.select').select2({
-    minimumResultsForSearch: Infinity,
-    dropdownCssClass: 'select-dropdown',
+  $('.select').each(function (idx, item) {
+    const name = $(this).attr('aria-placeholder')
+
+    $(this).select2({
+      minimumResultsForSearch: Infinity,
+      dropdownCssClass: 'select-dropdown',
+      placeholder: name,
+    })
   })
+
+  // $('.select').select2({
+  //   minimumResultsForSearch: Infinity,
+  //   dropdownCssClass: 'select-dropdown',
+  //   placeholder: $(this),
+  // })
 
   $('.select').on('select2:open', function () {
     $('.select2-results__options').niceScroll({
