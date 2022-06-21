71fcb213dfd9bf44faee08e0d4695e37c48f88f9
Anton Kiyan <kiyananton@gmail.com>
2022-06-21 15:50:26 +0300

    new content

--- source/js/main.js
+++ source/js/main.js
@@ -9,3 +9,10 @@ import './nav'
 import './quality-tabs'
 import './about-brand-tabs'
 import './modal-partnership'
+import './series-slider'
+
+$(window).on('load', function () {
+  if ($('.review__data-actions').length > 0 && $(window).width() < 768) {
+    $('.footer').css('padding-bottom', '70px')
+  }
+})
